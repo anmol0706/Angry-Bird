0ca2dc6c9d63e1b8d1e0427d044b5a99b1d3cbf1
anmol0706 <anmolmalviya4328@gmail.com>
2026-01-12 15:29:16 +0530

    Fix: Switch to Ubuntu 22.04 and add libffi-dev/pkg-config to resolve build failures

--- setup_linux.sh
+++ setup_linux.sh
@@ -35,7 +35,8 @@ sudo apt install -y python3-pip build-essential git python3 python3-dev \
     ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
     libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev \
     libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
-    autoconf automake libtool
+    autoconf automake libtool gettext autopoint \
+    libffi-dev pkg-config
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}✓ System dependencies installed${NC}"
